5b8c890b0648c3b4c42af754f792daba5e93b2dd
linhao <linhao@galasports.com>
2020-05-26 17:19:15 +0800

    finish

--- week06/mealy-kmp.js
+++ week06/mealy-kmp.js
@@ -1,92 +1,4 @@
-/**
- * 计算 模式串中 前后缀交集元素的最大长度
- * @param {*} d 
- */
-function calcLen(d) {
-	if (d.length === 1 || d.length === 2) {
-		return 0
-	} else {
-		return compareLen(leftPart(d), rightPart(d))
-	}
-}
-/**
- * 前缀
- * @param {} d 
- */
-function leftPart(d) {
-	let arr = []
-	let temp = ''
-	for (var k = 0; k < d.length - 1; k++) {
-		temp += d[k]
-		arr.push(temp)
-	}
-	return arr
-}
-/**
- * 后缀
- * @param {} d 
- */
-function rightPart(d) {
-	let arr = []
-	let temp = ''
-	for (var k = d.length - 1; k > 0; k--) {
-		temp = d[k] + temp
-		arr.push(temp)
-	}
-	return arr
-}
-function compareLen(d1, d2) {
-	// 匹配交集
-	let ret = []
-	for (let n = 0; n < d1.length; n++) {
-		for (let m = 0; m < d2.length; m++) {
-			if (d2[m] === d1[n] && !ret.includes(d2[m])) {
-				ret.push(d2[m])
-			}
-		}
-	}
-	// 获取最大长度
-	if (ret.length > 0) {
-		ret = ret.map(m => m.split('').length)
-		return Math.max(...ret)
-	} else {
-		return 0
-	}
-}
-/**
- * 
- * @param {*} arr 数组
- * @param {*} offset 正整数，向右移n位，负整数，向左移n位
- * @param {*} pad 补数
- */
-function moveElement(arr, offset, pad) {
-	if (Math.abs(offset) > arr.length)
-		offset = offset % arr.length
-	if (pad)
-		return [pad].concat(arr.slice(0, -offset))
-	else
-		return arr.slice(-offset).concat(arr.slice(0, -offset))
-}
-/**
- * 根据pmt数组得到next数组
- * @param {*} d 
- */
-function next(d) {
-	let arr = d.split('')
-	let char = arr[0]
-	let knife = 0;
-	let MaxL = []
-	let _next;
-	while (knife < arr.length) {
-		let len = calcLen(char)
-		MaxL.push(len)
-		knife++
-		char = arr.slice(0, knife + 1)
-	}
-	_next = moveElement(MaxL, 1, -1)
-	// 向右移以为,第一位补-1
-	return _next
-}
+const next = require('./kmp1')
 /**
  * 
  * @param {*} string 主串
@@ -99,12 +11,15 @@ function next(d) {
 function match(string, pattern) {
 	let state = loop;
 	return function () {
-		for (let j = 0; j < pattern.length; j++) {
+		outer: for (let j = 0; j < pattern.length; j++) {
 			for (let i = 0; i < string.length; i++) {
 				state = state(string, pattern, i, j);
 				j = this.j
 				if (state === fin) {
 					break;
+				}
+				if (state === kill) {
+					break outer;
 				}
 			}
 		}
@@ -137,12 +52,21 @@ function loop(string, pattern, i, j) {
  */
 function replacePosition(string, pattern, i, j) {
 	this.j = _next[j];
+	/**
+ * 1. 当模式串和主串的匹配到最后一位，发现匹配不了, 心想坑爹啊，真是浪费表情
+ * 2. 当主串匹配到一半，发现后面不可能再也不可能跟模式串匹配成功，想起第一种情况，就及时止损出局了
+ */
+	if ((i === string.length - 1 && j < pattern.length) || (string.length - i < pattern.length)) {
+		console.log(`匹配结束，未能匹配成功！`)
+		return kill
+	}
 	if (this.j > 0 && pattern[this.j] !== string[i]) {
 		return replacePosition(string, pattern, i, this.j)
 	}
 	if (string[i] === pattern[this.j]) {
 		return matchNext(string, pattern, i, this.j);
 	}
+
 	return loop;
 }
 /**
@@ -157,19 +81,24 @@ function alreadyMatched(string, pattern, i, j) {
 	console.log(`匹配到了, 位置在区间 [${i - pattern.length + 1}, ${i + 1}]`)
 	return fin;
 }
-/**
- * 结束
- */
+
 function fin() {
 	return fin
 }
-// var string = 'abaabaabbabaaabaabbabaab';
-// var pattern = 'abaabbabaab';
+function kill() {
+	return kill
+}
+var string = 'abaabaabbabaaabaabbabaab';
+var pattern = 'abaabbabaab';
 // var string = 'abcabcab';
 // var pattern = 'cab';
 // var string = 'abcabcabx';
 // var pattern = 'abx';
-var string = 'acabaabaabcaccaabc';
-var pattern = 'abaabcac';
+// var string = 'acabaabaabcaccaabc';
+// var pattern = 'abaabcac';
+// var string = 'abcabc'
+// var pattern = 'acx'
+// var string = 'abcabc'
+// var pattern = 'abx'
 var _next = next(pattern);
 match(string, pattern);
