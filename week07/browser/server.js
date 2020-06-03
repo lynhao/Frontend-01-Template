465cf736dce73b153bbf71351499c19fa16b9e0f
linhao <linhao@foresee.com.cn>
2020-06-03 09:12:30 +0800

    beta

--- week07/browser/server.js
+++ week07/browser/server.js
@@ -9,6 +9,21 @@ const server = http.createServer((req, res) => {
     res.end(`<html>
 <head>
 <style>
+#container {
+    width:500px;
+    height:300px;
+    display:flex;
+    background-color: rgb(255,255,255);
+}
+#container #myid {
+    width: 200px;
+    height: 100px;
+    background-color:rgb(255,0,0);
+}
+#container .c1 {
+    flex: 1;
+    background-color:rgb(0,255,0);
+}
 #myid ~ .c1 {
     outline: solid red 1px;
 }
